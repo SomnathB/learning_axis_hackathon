04a8c0d66c795b823b909863068cd2dbc029f173
Himanshu Gupta <himanshu.mps@gmail.com>
2021-02-01 04:02:18 +0530

    Update s2i-buildah.sh

--- s2i-buildah.sh
+++ s2i-buildah.sh
@@ -1,7 +1,7 @@
 echo "=================== Generating s2i for buildah ==================="
 s2i build  --as-dockerfile Dockerfile https://github.com/himanshumps/vertx-starter.git quay.io/himanshumps/ubi_java8 vertx_demo
 echo "=================== Running s2i with buildah ==================="
-buildah bud  --layers -t quay.io/himanshumps/vertx_demo .
+buildah bud  -v maven/:/tmp/maven/settings/:Z --layers -t quay.io/himanshumps/vertx_demo .
 echo "=================== Listing the images ==================="
 buildah images
 echo "=================== Pushing the image to quay registry ==================="
