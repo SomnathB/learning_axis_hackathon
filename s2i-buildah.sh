c92e6846b813d84273be9801ffb92c139c412d7e
Himanshu Gupta <himanshu.mps@gmail.com>
2021-02-01 04:26:24 +0530

    Update s2i-buildah.sh

--- s2i-buildah.sh
+++ s2i-buildah.sh
@@ -1,11 +1,11 @@
 echo "=================== Generating s2i for buildah ==================="
 s2i build  --as-dockerfile Dockerfile https://github.com/himanshumps/vertx-starter.git quay.io/himanshumps/ubi_java8 vertx_demo
 echo "=================== Running s2i with buildah ==================="
-#buildah bud  -v $PWD/maven/:/tmp/maven/settings/:Z --layers -t quay.io/himanshumps/vertx_demo .
+buildah bud  -v $PWD/maven/:/tmp/maven/settings/:Z --layers -t quay.io/himanshumps/vertx_demo .
 echo "=================== Listing the images ==================="
-#buildah images
+buildah images
 echo "=================== Pushing the image to quay registry ==================="
-#buildah push quay.io/himanshumps/vertx_demo:latest
+buildah push quay.io/himanshumps/vertx_demo:latest
 echo "=================== Logging in openshift ==================="
 oc login --insecure-skip-tls-verify --token=kUch6fTfQ6C6bFZ2QdhlGt2NMfKpJIgbrFNen2WMIj4 --server=https://api.shared-na4.na4.openshift.opentlc.com:6443
 echo "=================== Updating the imagestream for automated deployment ==================="
